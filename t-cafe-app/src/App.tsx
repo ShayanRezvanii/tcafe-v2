ec1e0078774c2a7f89f54712dc0aa571d1c1d010
ShayanRezvani <shayanrezvani5@gmail.com>
2024-02-25 19:27:48 +0330

    🎨 Import Sctructre!

---
+++ t-cafe-app/src/App.tsx
@@ -0,0 +1,10 @@
+/** @format */
+
+import "./App.css";
+import MainLayout from "./Components/Layout/MainLayout";
+
+function App() {
+  return <MainLayout></MainLayout>;
+}
+
+export default App;
---
+++ t-cafe-app/src/Components/Layout/MainLayout.tsx
@@ -0,0 +1,8 @@
+/** @format */
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function MainLayout({ children }: any) {
+  return <div className=" w-full min-h-screen overflow-hidden">{children}</div>;
+}
+
+export default MainLayout;
